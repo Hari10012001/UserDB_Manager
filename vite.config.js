bbeb7053d0f734a76eb227b5cd490687a3ef0430
HARIHARAN P <hariharanp10012001@gmail.com>
2025-06-10 22:43:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/UserDB_Manager/", // 👈 this line is required
+  base: "/UserDB_Manager/", 
 })
